abb0ac199acebae7b314ed8b0e4de700813dff7a
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2024-10-14 11:54:17 -0500

    MsContentModeratorApplicationTests.java

--- src/test/java/pe/edu/vallegrande/app/MsContentModeratorApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/app/MsContentModeratorApplicationTests.java
@@ -2,12 +2,15 @@ package pe.edu.vallegrande.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest
 class MsContentModeratorApplicationTests {
 
-	/*@Test
+	@Test
 	void contextLoads() {
-	}*/
+	    log.info("default test");
+	}
 
 }
