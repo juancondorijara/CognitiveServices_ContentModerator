6c87872b7e8b24253feb4c8b8afc194d4ef88e76
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2024-11-18 09:59:14 -0500

    [LOW]: environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   contentModerator_be: 'https://silver-space-orbit-q6rp6wpvwv5f6775-8085.app.github.dev/ms_content_moderator'
+  //contentModerator_be: '${API_BACKEND}'
 };
